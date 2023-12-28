3f8d614886a2df28e8536de8d1029d2bc877c4fb
OlegKovrigaDev <41855503+OlegKovrigaDev@users.noreply.github.com>
2023-12-28 14:20:49 +0000

    Deploying to gh-pages from @ OlegKovrigaDev/goit-react-hw-08-phonebook@ea0d996466754bbbfbd194b75ef3feabd79e3001 🚀

---
+++ static/js/415.16d8f455.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,o){o.r(t);var r=o(3329);t.default=function(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{children:"Home page of the Phonebook"})})}}}]);
+//# sourceMappingURL=415.16d8f455.chunk.js.map
